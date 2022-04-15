3599772813f6018d69f238c3495226cf2fb2ded6
RealCryZed <bujhmgtnhjd@bk.ru>
2022-04-15 19:27:35 +0600

    First commit. Added application.properties

---
+++ src/main/java/com/techshark/TechsharkApplication.java
@@ -0,0 +1,13 @@
+package com.techshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsharkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techshark/TechsharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsharkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

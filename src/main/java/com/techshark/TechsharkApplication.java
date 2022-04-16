0c1eaa0f71d3bc7e5433d212c0e6f0c28f99a900
RealCryZed <bujhmgtnhjd@bk.ru>
2022-04-16 22:03:32 +0600

    Added main page, added ViewResolver for Spring

--- src/main/java/com/techshark/TechsharkApplication.java
+++ src/main/java/com/techshark/TechsharkApplication.java
@@ -2,6 +2,9 @@ package com.techshark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @SpringBootApplication
 public class TechsharkApplication {
@@ -10,4 +13,13 @@ public class TechsharkApplication {
 		SpringApplication.run(TechsharkApplication.class, args);
 	}
 
+	@Bean
+	public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("classpath:templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCacheable(false); //make cacheable in future
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		return templateResolver;
+	}
 }
